4ad1b173d3a674666763e45725271c35818570ad
billymills <wmills@unca.edu>
2012-12-02 19:21:32 -0500

    implemented destructor

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -14,17 +14,17 @@ Hash<V>::Hash(){
 
 template <typename V>
 Hash<V>::~Hash(){
-	cout << "in dest" << endl;
 	for (int i = 0; i < table.size(); ++i){
-		cout << "in for" << endl;
 		if (table[i].size() != 0){
 			for (int i = 0; i < table[i].size(); ++i){
 				table[i].pop_back();
 			}
 		}
 	}
-	cout << "out" << endl;
-	//delete &table;
+
+	for (int i = 0; i < table.size(); ++i){
+		table.pop_back();
+	}
 }
 
 template <typename V>
